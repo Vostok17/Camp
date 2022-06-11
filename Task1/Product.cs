75ad0dc2c2a55ac5189c02c4eda645c60f494b64
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-06-11 03:31:43 +0300

    Task1

---
+++ Task1/Product.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Product
+    {
+        #region Fields and props
+
+        public int? Id { get; init; }
+        public string Name { get; init; }
+
+        private decimal _price;
+        public decimal Price
+        {
+            get { return _price; }
+            init
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Price must be greater than zero.");
+                _price = value;
+            }
+        }
+
+        private double _weight;
+        public double Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Weight must be greater than zero.");
+                _weight = value;
+            }
+        }
+
+        #endregion
+
+        public Product() { }
+        public Product(int id, string name, decimal price, double weight)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+
+        #region Object methods
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Price: {Price}, Weight: {Weight}.";
+        }
+
+        /// <summary>
+        /// Products are equal when they have the same Id.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj is Product other)
+            {
+                return Id == other.Id;
+            }
+            return false;
+        }
+        public override int GetHashCode() => Name.GetHashCode();
+
+        #endregion
+
+        public void Deconstruct(out int? id, out string name, out decimal price, 
+            out double weight)
+        {
+            id = Id;
+            name = Name;
+            price = _price;
+            weight = _weight;
+        }
+
+        public static bool operator ==(Product p1, Product p2)
+            => p1.Equals(p2);
+        public static bool operator !=(Product p1, Product p2)
+            => !p1.Equals(p2);
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using Task1;
+
+#region Create Product instances
+
+Product apple = new Product
+{
+    Id = 0,
+    Name = "Red apple",
+    Price = 12.5m,
+    Weight = 3
+};
+
+Product banana = new Product(1, "Yellow banana", price: 24m, weight: 1);
+
+Product orange = new Product(1, "Orange orange :D", price: 34.5m, weight: 0.5)
+{
+    Id = 2
+};
+
+List<Product> products = new List<Product>
+{
+    apple,
+    banana
+};
+
+#endregion
+
+#region Buy (Add, Remove)
+
+Buy basket = new Buy(apple, banana);
+basket.Add(products);
+basket.Add(orange);
+
+Console.WriteLine(basket);
+
+basket.Remove(0, 1);
+basket.Remove(2, 2);
+
+Console.WriteLine(basket);
+
+#endregion
+
+#region Check
+
+Check check = new Check(basket);
+Console.WriteLine(check);
+
+#endregion
+
+Console.ReadKey();
