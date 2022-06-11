0f111e194d4fb98f0b84fb8995ae1004ef3fe221
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-06-11 12:41:31 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿using Task3;
+
+#region Random Initialization and Reverse
+
+Vector v = new Vector(10);
+v.RandomInitialization(1, v.Lenght);
+Console.WriteLine("Initialized vector:\n" + v + '\n');
+
+v.Reverse();
+Console.WriteLine("Reversed vector:\n" + v + '\n');
+
+#endregion
+
+#region IsPalindrome and FindLongestSequence
+
+Vector palindrome = new Vector(new int[] { 1, 2, 3, 2, 1 });
+Console.WriteLine(palindrome);
+Console.WriteLine("Is palindrome? {0}", palindrome.IsPalindrome());
+
+Console.WriteLine("\nFind longest sequence:");
+Vector testVector = new Vector(new int[] { 9, 2, 4, 4, 5, 4, 4, 4, 5, 4, 3, 3, 2 });
+Console.WriteLine(testVector);
+Console.WriteLine(testVector.FindLongestSequence());
+
+#endregion
+
+#region InitShuffle and CalculateFrequency
+
+Vector vecToShuffle = new Vector(20);
+vecToShuffle.InitShuffle();
+Console.WriteLine("\nShuffled array:\n" + vecToShuffle);
+
+Console.WriteLine($"\nFrequency for testVector:\n" + testVector);
+Pair[] freq = testVector.CalculateFrequency();
+foreach (Pair pair in freq)
+    Console.WriteLine(pair);
+
+#endregion
+
+#region Diagonal snake with direction
+
+Console.WriteLine("\nDiagonal snake with direction:");
+Matrix matrix = new Matrix(4);
+
+Console.WriteLine("\nUp right:\n");
+matrix.DiagonalSnake(DiagonalShakeDirectionEnum.UpRight);
+Console.WriteLine(matrix);
+
+Console.WriteLine("Left down:\n");
+matrix.DiagonalSnake(DiagonalShakeDirectionEnum.LeftDown);
+Console.WriteLine(matrix);
+
+#endregion
+
+Console.ReadKey();
---
+++ Task3/Vector.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Vector
+    {
+        private int[] _array;
+        public int Lenght => _array.Length;
+
+        public Vector() { }
+        public Vector(int n)
+        {
+            _array = new int[n];
+        }
+        public Vector(int[] arr)
+        {
+            _array = arr;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _array.Length)
+                    throw new IndexOutOfRangeException(
+                        "Index must be greater than or equal to zero and less than lenght.");
+                return _array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _array.Length)
+                    throw new IndexOutOfRangeException(
+                        "Index must be greater than or equal to zero and less than lenght.");
+                _array[index] = value;
+            }
+        }
+
+        #region Object methods
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Vector other)
+            {
+                return ToString() == other.ToString();
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+        public override string ToString()
+        {
+            return string.Join(' ', _array);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void RandomInitialization(int min, int max)
+        {
+            Random random = new Random();
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = random.Next(min, max);
+            }
+        }
+        public void InitShuffle()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = i;
+            }
+            Random random = new Random();
+            _array = _array.OrderBy(x => random.Next()).ToArray();
+        }
+        public Pair[] CalculateFrequency()
+        {
+            var res = _array
+                .GroupBy(x => x)
+                .Select(x => new Pair(x.Key, x.Count()))
+                .ToArray();
+
+            return res;
+        }
+        public bool IsPalindrome()
+        {
+            bool isPalindrom = true;
+            for (int i = 0; i < _array?.Length / 2; i++)
+            {
+                if (_array[i] != _array[_array.Length - 1 - i])
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            return isPalindrom;
+        }
+        public void Reverse()
+        {
+            int lenght = _array.Length;
+
+            for (int i = 0; i < lenght / 2; i++)
+            {
+                (_array[i], _array[lenght - 1 - i]) = (_array[lenght - 1 - i], _array[i]);
+            }
+        }
+        public string FindLongestSequence()
+        {
+            int? resNum = null;
+            int maxCount = 1;
+            int count = 1;
+
+            for (int i = 0; i < _array.Length - 1; i++)
+            {
+                if (_array[i] == _array[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        resNum = _array[i];
+                        maxCount = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (resNum != null)
+                return $"Number {resNum} occurs {maxCount} times in a row.";
+            else
+                return "Every number occurs only once.";
+        }
+
+        #endregion
+    }
+}
