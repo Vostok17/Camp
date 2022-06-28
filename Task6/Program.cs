dd3f1a100edc259f9eeface35e0295ac4fedfdaf
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-06-28 18:03:32 +0300

    Table tested

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,7 +1,26 @@
 ﻿using Task6;
 
-ElectricityMetering em = new ElectricityMetering("quarter.csv");
-em.Fill();
+ElectricityMetering em = new ElectricityMetering("quarter.txt");
+//em.FillData();
+
+//em.SaveToJson();
+//em.SaveToFile();
+
+em.Read();
+
+em.SaveToFile();
+
+var antype = new { namedddddddddddddddddddd = "hellodfgdfg", time = DateTime.Now };
+//var test2 = antype with { name = "done" };
+
+var list = new List<object>();
+
+list.Add(antype);
+//list.Add(test2);
+
+var table = new Table(list.ToArray());
+
+Console.WriteLine(table);
 
 Console.WriteLine("Success!");
 Console.ReadKey();
