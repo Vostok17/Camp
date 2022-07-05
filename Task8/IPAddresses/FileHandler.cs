6268fcb0a531f90695a0957d3fb8bc5a6fa6d797
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-07-05 11:50:30 +0300

    Add task with IPs

---
+++ Task8/IPAddresses/FileHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAddresses
+{
+    internal class FileHandler
+    {
+        private string _path;
+
+        public FileHandler() { }
+        public FileHandler(string filename)
+        {
+            string assets = @"../../../assets";
+            _path = Path.GetFullPath(Path.Combine(assets, filename));
+        }
+        public List<(string ip, TimeOnly time, DayOfWeek day)> Read()
+        {
+            var data = new List<(string ip, TimeOnly time, DayOfWeek day)>();
+
+            using (var sr = new StreamReader(_path))
+            {
+                // Skip header.
+                sr.ReadLine();
+
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(";");
+                    var day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), parts[2], true);
+                    data.Add((parts[0], TimeOnly.Parse(parts[1]), day));
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ Task8/IPAddresses/Program.cs
@@ -0,0 +1,43 @@
+﻿using IPAddresses;
+
+FileHandler file = new FileHandler("data.csv");
+
+var data = file.Read();
+
+Console.WriteLine("Raw data:");
+foreach (var item in data)
+{
+    Console.WriteLine($"{item.ip} - {item.time.ToLongTimeString()} - {item.day}");
+}
+Console.WriteLine();
+
+Statistics statistics = new Statistics(data);
+
+Console.WriteLine("Count of visits:");
+foreach (var item in statistics.GetCountOfVisits())
+{
+    Console.WriteLine($"{item.ip} - {item.count}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Most popular days:");
+foreach (var item in statistics.GetTheMostPopularDays())
+{
+    Console.WriteLine($"{item.ip} - {item.day}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Most active hours for IPs:");
+foreach (var item in statistics.GetTheMostActiveHours())
+{
+    Console.WriteLine($"{item.ip}: {item.time} - {item.time.AddHours(1)}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Most active hours for days:");
+foreach (var item in statistics.GetTheMostActiveHoursForSite())
+{
+    Console.WriteLine($"{item.day}: {item.time} - {item.time.AddHours(1)}");
+}
+
+Console.ReadKey();
