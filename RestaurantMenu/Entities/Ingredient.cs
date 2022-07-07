1ff145bbf7069121e124b83621be184f1f97fc91
Gagarin Artem <gagarin.artem.2008@gmail.com>
2022-07-07 12:33:10 +0300

    Add input files, dish and ingredients

---
+++ RestaurantMenu/Entities/Ingredient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu.Entities
+{
+    internal class Ingredient
+    {
+        public string Name { get; init; }
+        public int Weight { get; init; }
+
+        public Ingredient() { }
+        public Ingredient(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        #region ObjectsMethods
+        public override string ToString()
+        {
+            return $"{Name}, {Weight}.";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Ingredient other)
+            {
+                return Name == other.Name;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        #endregion
+    }
+}
